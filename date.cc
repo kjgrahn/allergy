d9b086c5b9fec91c8a7ef338a418f78f0c667919
Jörgen Grahn <grahn+src@snipabacken.se>
2013-07-17 14:08:03 +0200

    more work on dates

--- date.cc
+++ date.cc
@@ -4,34 +4,158 @@
  */
 #include "date.h"
 
+#include "blob.h"
+
+#include <cstring>
+
 namespace {
 
-    static const tm NIL_TM = { 0 };
+    /**
+     * Advance past a Mon--Sun or Monday--Sunday.
+     * XXX The latter missing.
+     */
+    const char* weekday(const char* a, const char* const b)
+    {
+	static const char names[] = "Mon Tue Wed Thu Fri Sat Sun";
+	if(b-a < 3) return a;
+	int found = 0;
+	for(int i=0; i<7; i++) {
+	    found += !std::strncmp(a, names+4*i, 3);
+	}
+	if(found) a+=3;
+	return a;
+    }
 
-};
+
+    /**
+     * Parse a three-letter month name in a[0..2] as Jan--Dec,
+     * into 0--11, or -1.
+     */
+    int month(const char* a)
+    {
+	static const char names[] =
+	    "Jan Feb Mar Apr May Jun "
+	    "Jul Aug Sep Oct Nov Dec";
+	int n = -1;
+	for(int i=0; i<12; i++) {
+	    if(std::strncmp(a, names+4*i, 3)==0) n = i;
+	}
+	return n;
+    }
+
+
+    unsigned dd(const char* p)
+    {
+	return 10*(p[0] - '0') + (p[1] - '0');
+    }
+
+    unsigned xd(const char* p)
+    {
+	if(p[0]==' ') return p[1] - '0';
+	return dd(p);
+    }
+
+    unsigned dddd(const char* p)
+    {
+	return 100*dd(p) + dd(p+2);
+    }
+
+
+    bool parse_rfc850(const char* a, const char* const b,
+		      struct tm& t)
+    {
+	/*   Sunday, 06-Nov-94 08:49:37 GMT
+	 * Saturday, 06-Nov-94 08:49:37 GMT
+	 *           ^cut here
+	 */
+	if(b-a < 30) return false;
+	while(a!=b && *a!= ' ') a++;
+	if(a==b) return false;
+	a++;
+	if(a[ 2] != '-' ||
+	   a[ 6] != '-' ||
+	   a[ 9] != ' ' ||
+	   a[12] != ':' ||
+	   a[15] != ':' ||
+	   a[18] != ' ') return false;
+	t.tm_mday = dd(a + 0);
+	t.tm_mon  = month(a + 3);
+	t.tm_year = dd(a + 7);
+	/* This sucks, but she should have used a four-digit format: */
+	if(t.tm_year<70) t.tm_year += 100;
+	t.tm_hour = dd(a + 10);
+	t.tm_min  = dd(a + 13);
+	t.tm_sec  = dd(a + 16);
+	return true;
+    }
+
+    bool parse_asctime(const char* const a, const char* const b,
+		       struct tm& t)
+    {
+	/* Sun Nov  6 08:49:37 1994 */
+	if(b-a != 24) return false;
+	if(a[ 3] != ' ' ||
+	   a[ 7] != ' ' ||
+	   a[10] != ' ' ||
+	   a[13] != ':' ||
+	   a[16] != ':' ||
+	   a[19] != ' ') return false;
+	t.tm_mon  = month(a + 4);
+	t.tm_mday = xd(a + 8);
+	t.tm_hour = dd(a + 11);
+	t.tm_min  = dd(a + 14);
+	t.tm_sec  = dd(a + 17);
+	t.tm_year = dddd(a + 20) - 1900;
+	return true;
+    }
+
+
+    /**
+     * Parse a date, while filling in a broken-down representation.
+     */
+    bool parse(const char* const a, const char* const b,
+	       struct tm& t)
+    {
+	/* Sun, 06 Nov 1994 08:49:37 GMT */
+	if(b-a != 29) return parse_rfc850(a, b, t) || parse_asctime(a, b, t);
+	if(a[ 3] != ',' ||
+	   a[ 4] != ' ' ||
+	   a[ 7] != ' ' ||
+	   a[11] != ' ' ||
+	   a[16] != ' ' ||
+	   a[19] != ':' ||
+	   a[22] != ':' ||
+	   a[25] != ' ') return false;
+
+	t.tm_mday = dd(a + 5);
+	t.tm_mon  = month(a + 8);
+	t.tm_year = dddd(a + 12) - 1900;
+	t.tm_hour = dd(a + 17);
+	t.tm_min  = dd(a + 20);
+	t.tm_sec  = dd(a + 23);
+	return true;
+    }
+}
 
 
 /**
- * Parse a HTTP date.  [3.3.1] lists these examples, where the first
- * is the recommended:
- *
- *    Sun, 06 Nov 1994 08:49:37 GMT  ; RFC 822, updated by RFC 1123
- *    Sunday, 06-Nov-94 08:49:37 GMT ; RFC 850, obsoleted by RFC 1036
- *    Sun Nov  6 08:49:37 1994       ; ANSI C's asctime() format
+ * Parse a HTTP date.
  *
  */
 time_t DateConv::parse(const std::string& s)
 {
-    const char* const p = s.c_str();
-    struct tm tm = NIL_TM;
+    return parse(Blob(s));
+}
 
-    const char* q = strptime(p, "%a, %d %b %Y %T GMT", &tm);
-    if(!q) {
-	q = strptime(p, "%a, %d-%b-%y %T GMT", &tm);
-    }
-    if(!q) {
-	q = strptime(p, "%a %b %d %T %Y", &tm);
-    }
 
-    return q? mktime(&tm) : 0;
+time_t DateConv::parse(const Blob& s)
+{
+    time_t t = 0;
+    struct tm tm;
+    const char* p = reinterpret_cast<const char*>(s.begin());
+    if(::parse(p, p + s.size(), tm)) {
+	tm.tm_isdst = 0;
+	t = timegm(&tm);
+    }
+    return t;
 }
--- test/test_date.cc
+++ test/test_date.cc
@@ -13,6 +13,17 @@ namespace {
      * Thu May  9 09:35:01 UTC 2013
      */
     const time_t may9 = 1368092101;
+
+    void assert_parses(const char* s, time_t t)
+    {
+	DateConv dc;
+	testicle::assert_eq(dc.parse(s), t);
+    }
+
+    void assert_formats(const char* s, time_t t)
+    {
+	testicle::assert_neq(0, 0);
+    }
 }
 
 
@@ -20,11 +31,76 @@ namespace date {
 
     using testicle::assert_eq;
 
-    void test_simple()
-    {
-	DateConv dc;
-	assert_eq(dc.parse("Thu, 09 May 2013 09:35:01 GMT"), may9);
-	assert_eq(dc.parse("Thursday, 09-May-13 09:35:01 GMT"), may9);
-	assert_eq(dc.parse("Thu May  9 09:35:01 2013"), may9);
+    namespace parse {
+
+	void test_simple()
+	{
+	    DateConv dc;
+	    assert_eq(dc.parse("Thu, 09 May 2013 09:35:01 GMT"), may9);
+	    assert_eq(dc.parse("Thursday, 09-May-13 09:35:01 GMT"), may9);
+	    assert_eq(dc.parse("Thu May  9 09:35:01 2013"), may9);
+	}
+
+	void test_ignore_wd()
+	{
+	    DateConv dc;
+	    assert_eq(dc.parse("Mon, 09 May 2013 09:35:01 GMT"), may9);
+	    assert_eq(dc.parse("Monday, 09-May-13 09:35:01 GMT"), may9);
+	    assert_eq(dc.parse("Mon May  9 09:35:01 2013"), may9);
+	}
+
+	void test_leading_zero_asctime()
+	{
+	    DateConv dc;
+	    assert_eq(dc.parse("Thu May  9 09:35:01 2013"), may9);
+	    assert_eq(dc.parse("Thu May 09 09:35:01 2013"), may9);
+	}
+
+	void test_various()
+	{
+	    assert_parses("Fri, 01 Jan 1970 00:00:00 GMT", 0);
+	    assert_parses("Fri, 01 Jan 1970 00:00:01 GMT", 1);
+	    assert_parses("Fri, 01 Jan 1970 00:01:00 GMT", 60);
+	    assert_parses("Fri, 01 Jan 1970 01:00:00 GMT", 3600);
+	    assert_parses("Fri, 01 Jan 1970 06:02:00 GMT", 21720);
+	    assert_parses("Fri, 01 Jan 1970 23:00:00 GMT", 82800);
+	    assert_parses("Fri, 31 Jan 1970 06:02:00 GMT", 2613720);
+	    assert_parses("Fri, 31 Dec 1999 06:02:00 GMT", 946620120);
+	    assert_parses("Fri, 01 Jan 2000 06:02:00 GMT", 946706520);
+	    assert_parses("Fri, 28 Feb 2000 06:02:00 GMT", 951717720);
+	    assert_parses("Fri, 29 Feb 2000 06:02:00 GMT", 951804120);
+	    assert_parses("Fri, 01 Mar 2000 06:02:00 GMT", 951890520);
+	    assert_parses("Fri, 01 Jun 2000 06:02:00 GMT", 959839320);
+	    assert_parses("Tue, 19 Jan 2038 03:10:00 GMT", 2147483400);
+	    assert_parses("Fri, 01 Mar 2048 06:02:00 GMT", 2466655320);
+	}
+    }
+
+    namespace format {
+
+	void test_simple()
+	{
+	    DateConv dc;
+	    assert_formats("Thu, 09 May 2013 09:35:01 GMT", may9);
+	}
+
+	void test_various()
+	{
+	    assert_formats("Fri, 01 Jan 1970 00:00:00 GMT", 0);
+	    assert_formats("Fri, 01 Jan 1970 00:00:01 GMT", 1);
+	    assert_formats("Fri, 01 Jan 1970 00:01:00 GMT", 60);
+	    assert_formats("Fri, 01 Jan 1970 01:00:00 GMT", 3600);
+	    assert_formats("Fri, 01 Jan 1970 06:02:00 GMT", 21720);
+	    assert_formats("Fri, 01 Jan 1970 23:00:00 GMT", 82800);
+	    assert_formats("Fri, 31 Jan 1970 06:02:00 GMT", 2613720);
+	    assert_formats("Fri, 31 Dec 1999 06:02:00 GMT", 946620120);
+	    assert_formats("Fri, 01 Jan 2000 06:02:00 GMT", 946706520);
+	    assert_formats("Fri, 28 Feb 2000 06:02:00 GMT", 951717720);
+	    assert_formats("Fri, 29 Feb 2000 06:02:00 GMT", 951804120);
+	    assert_formats("Fri, 01 Mar 2000 06:02:00 GMT", 951890520);
+	    assert_formats("Fri, 01 Jun 2000 06:02:00 GMT", 959839320);
+	    assert_formats("Tue, 19 Jan 2038 03:10:00 GMT", 2147483400);
+	    assert_formats("Fri, 01 Mar 2048 06:02:00 GMT", 2466655320);
+	}
     }
 }
