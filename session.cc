9e11aed8eef8afc3b39cab1c0957c2b6ca8ec4bf
Jörgen Grahn <grahn+src@snipabacken.se>
2015-08-11 11:38:20 +0200

    log requests, not just whole client sessions

--- session.cc
+++ session.cc
@@ -94,6 +94,7 @@ Session::State Session::read(int fd, const timespec& t)
     if(req_queue.broken()) return DIE;
     if (req_queue.complete()) {
 	const Request req = req_queue.front();
+	Info(log) << *this << ' ' << req.method << ' ' << req.request_uri();
 	req_queue.pop();
 	response = new Response(req);
 	history.began(*response, t);
@@ -124,6 +125,7 @@ Session::State Session::write(int fd, const timespec& t)
 	    /* XXX how about broken()? */
 	    if (req_queue.complete()) {
 		const Request req = req_queue.front();
+		Info(log) << *this << ' ' << req.method << ' ' << req.request_uri();
 		req_queue.pop();
 		response = new Response(req);
 		history.began(*response, t);
