0f8c6af3d5a8220b215ad8d6b3fabcb2f7daa0fc
Jörgen Grahn <grahn+src@snipabacken.se>
2019-05-19 07:24:57 +0200

    entity::String

--- entity/string.h
+++ entity/string.h
@@ -7,8 +7,16 @@
 #ifndef GB_ENTITY_STRING_H_
 #define GB_ENTITY_STRING_H_
 
+#include <string>
+
+class Blob;
+
 namespace entity {
 
+    /**
+     * A fixed string as a HTML entity: a size, and a way to emit its
+     * content in reasonably-sized chunks.
+     */
     class String {
     public:
 	explicit String(const char* s);
@@ -17,6 +25,11 @@ namespace entity {
 	explicit String(const C& c)
 	    : String(c.str())
 	{}
+
+	size_t size() const { return s.size(); }
+	Blob tick();
+    private:
+	std::string s;
     };
 }
 
