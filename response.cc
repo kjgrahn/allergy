971e23e70e305c73ce2a2b4fb792c3aa6c60b340
Jörgen Grahn <grahn+src@snipabacken.se>
2020-07-24 11:22:58 +0200

    response.cc: remove cruft

--- response.cc
+++ response.cc
@@ -4,46 +4,6 @@
  */
 #include "response.h"
 
-#include "request.h"
-#include "filter.h"
-#include "date.h"
-
-#include <cctype>
-#include <sstream>
-
-
-namespace {
-
-    std::string rstrip(const std::string& s)
-    {
-	std::string r = s;
-
-	const char* const p = s.c_str();
-	const char* q = p + s.size();
-	while(p!=q) {
-	    if(!isspace(*(q-1))) break;
-	    q--;
-	}
-	r.resize(q-p);
-	return r;
-    }
-
-
-    DateConv dateconv;
-
-    std::string now()
-    {
-	return dateconv.format(time(0));
-    }
-
-
-    template <class Filter>
-    bool write(Filter& out, int fd, std::ostringstream& oss)
-    {
-	std::string s = oss.str();
-	return out.write(fd, Blob(s));
-    }
-}
 
 bool response::Headers::tick(int fd)
 {
